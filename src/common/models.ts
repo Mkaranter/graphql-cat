40404496b825342ecd0418313ca3e8f7669cd0a5
Michał Karanter <michal.karanter@gmail.comgit config --global user.name Micha>
2019-07-21 14:37:42 +0200

    passing arg

---
+++ src/common/models.ts
@@ -0,0 +1,3 @@
+export interface PlusTwo {
+	number: number;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,33 @@
-import express from "express";
-import graphqlHTTP from "express-graphql";
-import { buildSchema } from "graphql";
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+import { PlusTwo } from './common/models';
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     hello: String
+	random: Float!
+	plusTwo(number: Int): Int
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => "Hello world!"
+	hello: () => 'Hello world!',
+	random: () => Math.random(),
+	plusTwo: ({ number }: PlusTwo) => number + 2
 };
 
 const app = express();
 app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-  })
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true
+	})
 );
 app.listen(4000);
-console.log("Running a GraphQL API server at localhost:4000/graphql");
+console.log('Running a GraphQL API server at localhost:4000/graphql');
